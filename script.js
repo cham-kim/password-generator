069c95646bee180ba811105bfcdaabacf081c954
Cham Kim <chamkim@Chams-MacBook-Air.local>
2022-08-22 21:37:35 -0700

    This is a password generator

---
+++ script.js
@@ -0,0 +1,49 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+//Prompt for the length of the password
+var length = Number(prompt("How many characters will your password be? Enter a number between 8 and 128"));
+
+//Ask for a specific character type
+var charType = prompt("Enter a character type: special, numeric, uppercase, lowercase.");
+
+ //figure out the character type
+ var charSet = "";
+ var charTypeLower = charType.toLowerCase();
+ if( charTypeLower === "lowercase" ) {
+  charSet = "abcdefghijklmnopqrstuvwxyz";
+ } else if( charTypeLower === "uppercase" ) {
+  charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ } else if( charTypeLower === "numeric" ) {
+  charSet = "0123456789";
+ } else if( charTypeLower === "special" ) {
+  charSet = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+ }
+
+//This will generate the  password
+function generatePassword() {
+
+  //returns the value 
+  var returnVal = "";
+  for (var i = 0; i < length; i++) {
+
+    //selects the random characters
+    returnVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
+  }
+  return returnVal;
+}
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+
+
